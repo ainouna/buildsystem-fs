426e8104e0c8ee44c4b6462907cefc70d04e14d9
Frankenstone <dampf_acc@yahoo.com>
2017-10-21 10:26:33 +0200

    cosmetics no changes

--- make/patches.mk
+++ make/patches.mk
@@ -18,7 +18,6 @@
 %patch:
 	( cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ] )
 
-
 # keeping all patches together in one file
 # uncomment if needed
 #
