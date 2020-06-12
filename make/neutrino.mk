e6b8071773ee554bd00d9a670f5894b7502f7d5a
Frankenstone <dampf_acc@yahoo.com>
2020-06-12 15:25:47 +0200

    neutrino.mk: disable arm/mips ACC

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -101,8 +101,8 @@ N_CONFIG_OPTS += --disable-upnp
 
 ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
 N_CONFIG_OPTS += --enable-reschange
-#N_CONFIG_OPTS += --disable-arm-acc
-#N_CONFIG_OPTS += --disable-mips-acc
+N_CONFIG_OPTS += --disable-arm-acc
+N_CONFIG_OPTS += --disable-mips-acc
 endif
 
 ifeq ($(AUDIODEC), ffmpeg)
